f8901c99c611b23404659c460fd43845b97c2fe0
Pladque <63751303+Pladque@users.noreply.github.com>
2020-10-13 22:33:05 +0200

    first commit

---
+++ components/func.py
@@ -0,0 +1,14 @@
+
+
+def split_data(data, results, training_size):       #split data to test and learn, data to wejsc, result to wyjscie
+    pass
+
+
+def scale(data):    #scale data to 0 - 1
+    pass
+
+def model_fitness(data, results, model):     #returns how close model were
+    pass
+
+def train(model, input_data, output_data):        
+    pass
