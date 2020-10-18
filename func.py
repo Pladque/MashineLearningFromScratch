da31a56b885b208263d2e18acf0dfb0993d441fb
Pladque <63751303+Pladque@users.noreply.github.com>
2020-10-18 15:21:21 +0200

    some funcs

---
+++ func.py
@@ -0,0 +1,23 @@
+
+
+def split_data(data, results, training_size):       #split data to test and learn, data to wejsc, result to wyjscie
+    pass
+
+
+def scale(data):    #scale data to 0 - 1
+    pass
+
+def model_fitness(data, results, model):     #returns how close model were
+    pass
+
+def train(model, input_data, output_data):        
+    pass
+
+def adjust_weights(model):
+    pass
+
+def round_results(result, precision):  #precison: how many digits after coma. Adjust that value by analising data
+    pass
+
+def calculate_error_rate():
+    pass
