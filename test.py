096f74f499b835f37bd00d3176f4295e76cdc7c9
Pladque <63751303+Pladque@users.noreply.github.com>
2020-10-19 17:16:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ f = open("student-mat.csv", "r")
 
 
 x_train, y_train, x_test, y_test = split_data(f,["G1", "G2"],"G3", 0.1, ';')
-#testuj to jezcze mby
+
 print(x_train[0])
 print(y_train[0])
 print(x_test[0])
